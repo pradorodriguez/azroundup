532672b213bbd245df03032a1012f8981cdacfc7
Marco Prado <31007209+pradorodriguez@users.noreply.github.com>
2021-04-07 17:27:13 -0600

    Add project files.

---
+++ wa-github/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace wa_github.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
