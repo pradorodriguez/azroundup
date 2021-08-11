26b19395c7811f14ac1780292b94b9748cbdf559
Marco Prado <31007209+pradorodriguez@users.noreply.github.com>
2021-08-10 19:13:19 -0600

    Update Program.cs

--- wa-github/Program.cs
+++ wa-github/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Hosting;
+sing Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
